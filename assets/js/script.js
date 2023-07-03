66bc8afb6f08f26ae629bf6c44a2f63435d76fb4
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-07-03 18:44:39 -0300

    add transition menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ let scrollHeight = document.body.scrollHeight;
 
 
 
-function menuMobile(){
+function menuMobile(event){
+  if(event.type === 'touchstart') event.preventDefault();
   const nav = document.querySelector('.header-nav-menu');
   const body = document.body;
   nav.classList.toggle('active');
