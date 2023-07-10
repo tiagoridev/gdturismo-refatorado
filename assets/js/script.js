677bad50c39e554afba8ebe6478eb2708334448d
tiagoridev <tiagobr.desenvolvedor@gmail.com>
2023-07-10 16:05:17 -0300

    finish menu mobile actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 const btnMobile = document.querySelector('.btn-menu-mobile');
 let scrollHeight = document.body.scrollHeight;
 
-
-
 function menuMobile(event){
   if(event.type === 'touchstart') event.preventDefault();
   const nav = document.querySelector('.header-nav-menu');
@@ -14,37 +12,19 @@ function menuMobile(event){
     event.currentTarget.setAttribute('aria-label', 'Fechar menu');
   } else {
     event.currentTarget.setAttribute('aria-label', 'Abrir menu');
-  }
-  // if(nav.classList.contains('active')){
-  //   document.querySelector('main').style.visibility = 'hidden';
-  // } else {
-  //   document.querySelector('main').style.visibility = 'visible';
-  // }
-  
-  // if(nav.classList.contains('active')){
-  //   document.querySelector('.header-menu-list').style.height = scrollHeight + 'px';
-  // }
-  // if(nav.classList.contains('active')){
-  //   body.style.overflow = 'hidden'; 
-  // } else {
-  //   body.style.overflow = 'visible';
-  // }
+  }  
 }
-
 btnMobile.addEventListener('click', menuMobile)
 btnMobile.addEventListener('touchstart', menuMobile)
 
-const btnHistoria = document.querySelector('.btn-historia');
-const sectionHistoria = document.querySelector('.nossa-historia');
-let header = document.querySelector('header')
-
-
-function showHistoria() {
-  sectionHistoria.style.offsetHeight = header.offsetHeight + 200;
+const btnMenu = [...document.querySelectorAll('.header-menu-item')];
+function selectSection(){
+  const nav = document.querySelector('.header-nav-menu');
+  nav.classList.remove('active');
 }
-btnHistoria.addEventListener('click', showHistoria)
-
-
+btnMenu.forEach(btnMenuItem => {
+  btnMenuItem.addEventListener('click', selectSection)
+})
 
 const btnTop = document.querySelector('.btn-top-box')
 window.addEventListener('scroll', function(event){
